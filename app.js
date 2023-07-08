8e76b3282d343faf76e2fa29716b0b2b1ec7aabc
ugurgunes121 <139000448+ugurgunes121@users.noreply.github.com>
2023-07-08 19:23:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", async() => {
     const button = document.querySelector("#button")
 
-    const sw = await navigator.serviceWorker.register("http://127.0.0.1:5000/static/js/sw.js")
+    const sw = await navigator.serviceWorker.register("sw.js")
     console.log("service worker", sw)
     button.addEventListener( "click", async () => {
         const serviceWorker = await navigator.serviceWorker.ready;
